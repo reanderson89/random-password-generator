0422e0061c3fad908283401752531f82397c4910
Robert Anderson <reanderson89@gmail.com>
2020-09-23 08:35:59 -0400

    added comments to the script.js file and also adjusted the range of the random special characters to allow for the use of more characters in the passwords.

--- script.js
+++ script.js
@@ -9,74 +9,87 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// When the Generate Password button is clicked the writePassword function will run
 generateBtn.addEventListener("click", writePassword);
-// 
+
+// Function that runs the user through the possible options for their password.
 function promptUser() {
   var generatedPassword = "";
+  // Sets length of password
   var length = prompt("Choose a password length between 8 and 128 characters.");
-    
+    // If anything but a number is given the function will start over after alerting the user
   if (isNaN(length)) {
     alert("Please enter a number between 8 and 128");
     return promptUser();
+    // if cancel is clicked the prompt box will close
   } else if (!length) {
     return generatedPassword;
+    // if number is not within 8-128 alert the user
   } else if (length < 8 || length > 128) {
     alert("Length must be between 8 and 128.");
   } else {
+    // Asks the suer if they want numbers
     var numbers = confirm("Would you like the password to have numbers?");
-
+    // asks the user if they would like upper case letters 
     var upperCase = confirm(
       "Would you like the password to have UPPER CASE letters?"
     );
-
+      // asks the user if they would like lower case letters
     var lowerCase = confirm(
       "Would you like the password to have lower case letters?"
     );
-
+      // asks the user if they would like special characters
     var special = confirm(
       "Would you like the password to have special characters?"
     );
   }
+  // if length was chosen but all other items were turned down alert user and start over
   if (!numbers && !upperCase && !lowerCase && !special) {
     alert("You must select something!");
     return promptUser();
   } else {
+    // sets the generated password equal to the following functions and passes through the following variables to access
     generatedPassword = createPassword(numbers, upperCase, lowerCase, special, length);
   } 
   return generatedPassword;
 }
 
-
+// This function will generate our random password based off of the users input from promptUser function
 function createPassword (numbers, upperCase, lowerCase, special, length) {
   var functionArray = [];
-  //  Numbers
+  //  Generates a random number from 0-9
   var randomNumbers = function() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
     }
-   // Upper Case
+   // Generates a random upper case letter from A-Z
  var randomUpper = function() {
   return String.fromCharCode(Math.floor(Math.random()*26) + 65);
   }
-  // Lower Case
+  // Generates a random lower case letter from a-z
   var randomLower = function() {
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
   }
-  // Special
+  // Generates 1 of 4 different special characters
   var randomSpecial = function() {
-    return String.fromCharCode(Math.floor(Math.random()*4) + 35);
+    return String.fromCharCode(Math.floor(Math.random()*15) + 32);
   }
-
+  // creates an array of functions to be accessed later on
   var functionArray = [];
-
+  // Set variable to log numbers to ensure we don't later generate more characters then requested from the password
   var minimumCount = 0;
+  // each variable creates a blank string that can have characters applied to them as needed
   var guaranteedNumber = "";
   var guaranteedUpper = "";
   var guaranteedLower = "";
   var guaranteedSpecial = "";
 
+  // if the user chooses to have numbers
   if (numbers === true) {
+    // adds the randomNumbers function to our array of functions
     functionArray.push(randomNumbers);
+    // gives the guaranteedNumber variable one random number
     guaranteedNumber = randomNumbers();
+    // adds 1 to minimumCount
     minimumCount++;
   }
   if (upperCase === true) {
@@ -94,57 +107,24 @@ function createPassword (numbers, upperCase, lowerCase, special, length) {
     guaranteedSpecial = randomSpecial();
     minimumCount++;
   }
-
+  // creates a variable as an empty string that can later be returned with the users finished password
   var randomPasswordGenerated = "";
   
-
+  // for loop that runs as many times as the length of the password that was selected minus the minimumCount. The minimum count is subtracted because each time a number,upper case, lower case, special character was selected a random character from the selected option was added to the final password as a guarantee that the password would always have the options requested. Each pass of the loop generates a random character from the selected options and adds it to the randomPasswordGenerated variable.
   for (var i = 0; i < (parseInt(length) - minimumCount); i++) {
-    var choicesPicked = functionArray[Math.floor(Math.random() * functionArray.length)]();
+    var choicesPicked = functionArray[Math.floor(Math.random() * functionArray.length)](); 
     randomPasswordGenerated += choicesPicked;
   }
-
+  // whatever characters are inside each variable will be added to the users password. There will only be characters in them if they had been chosen by the user.
   randomPasswordGenerated += guaranteedNumber;
   randomPasswordGenerated += guaranteedSpecial;
   randomPasswordGenerated += guaranteedUpper;
   randomPasswordGenerated += guaranteedLower;
 
+  // returns the users randomly generated password
   return randomPasswordGenerated;
+  
 }
 
-// Add event listener to generate button
 
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-//  // Upper Case
-//  function() {
-//   return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-// },
-// // Lower Case
-// function() {
-//   return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-// },
-// // Numbers
-// function() {
-//   return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-// },
-// // Special
-// function() {
-//   return String.fromCharCode(Math.floor(Math.random()*4) + 35);
-// }
-
-// Math.floor(Math.random() * 1)
